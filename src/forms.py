648a9971838db15e005bca301cca30d983de083f
Abdul Motin <motin.mmk.jr@gmail.com>
2022-10-27 18:13:29 +0600

    from validetor

--- src/forms.py
+++ src/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
