edfd0476b7466d91da4d99a771dd87b5484cff5c
Abdul Motin <motin.mmk.jr@gmail.com>
2022-10-28 14:04:25 +0600

    sql alchemy database configuration with sqlite

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+# import dataclasses as dc
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'faequ227$_M'
+# sql alchemy database configuration with sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/site.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
+db = SQLAlchemy(app)
+
+
 from src import routes
 
 
