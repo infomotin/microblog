3f5a595fe71e38c8ad1f338b1e499cd8762d7683
Abdul Motin <motin.mmk.jr@gmail.com>
2022-10-28 14:58:36 +0600

    update asqlacademy

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
+# from flask_migrate import Migrate
 # import dataclasses as dc
 from flask_sqlalchemy import SQLAlchemy
+database_obj = SQLAlchemy()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'faequ227$_M'
 # sql alchemy database configuration with sqlite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/site.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
-db = SQLAlchemy(app)
+db = database_obj.init_app(app)
 from src import routes
 
 
