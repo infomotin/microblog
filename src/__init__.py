5b8cd6636c7be71e6774c38a83263d55c272ae71
Abdul Motin <motin.mmk.jr@gmail.com>
2022-10-28 15:23:50 +0600

    fixt

--- src/__init__.py
+++ src/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 # from flask_migrate import Migrate
 # import dataclasses as dc
 from flask_sqlalchemy import SQLAlchemy
-database_obj = SQLAlchemy()
+db = SQLAlchemy()
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'faequ227$_M'
 # sql alchemy database configuration with sqlite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/site.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
-db = database_obj.init_app(app)
+db.init_app(app)
+# migrate = Migrate(app, db)
 from src import routes
 
 
