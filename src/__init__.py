3dd50c60523fcae09b2be624138e46e9e7f192b8
Abdul Motin <motin.mmk.jr@gmail.com>
2022-10-29 15:18:37 +0600

    Working

--- src/__init__.py
+++ src/__init__.py
@@ -10,16 +10,19 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flask_db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
-    db.session.commit()
+# with app.app_context():
+#     db.create_all()
+#     db.session.commit()
 # show all tables in the database
 def show_tables():
     with app.app_context():
+        # db.create_all()
         tables = db.engine.execute("SHOW TABLES;")
         for table in tables:
             print(table)
             return tables
+        # db.session.commit()
+        
 # show all columns in a table in the database
 
 
@@ -33,3 +36,7 @@ from src import routes
 # baghorbari
 # Kulsum123
 # 161235150480
+# Emp_job_card_comp_part1_all.rep
+# Emp_job_card_comp_part2_all.rep
+# EmpJobCardCmpFormatAll.rpt
+# EmpCompJobCardAll.rpt
